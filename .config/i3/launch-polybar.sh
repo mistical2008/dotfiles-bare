598ee5e63c6e4abd0b4ab55d24355eb4e4b8babe
Evgeniy Talko <etalko@gmail.com>
2019-02-21 00:28:07 +0300

    Add i3 folder

---
+++ .config/i3/launch-polybar.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using custom config locatione
+polybar mybar -c ~/.config/i3/polybar.conf  -r &
+polybar mybar-2 -c ~/.config/i3/polybar.conf  -r &
+
+echo "Polybar launched..."
