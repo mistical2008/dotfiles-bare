560d1f1b52e60ccf830868d2395bcedba9df062f
Evgeniy Talko <etalko@gmail.com>
2019-06-20 14:13:24 +0300

    added ~/.xprofile and edits in launch-polybar.sh

--- .config/i3/launch-polybar.sh
+++ .config/i3/launch-polybar.sh
@@ -7,7 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using custom config locatione
-polybar mybar -c ~/.config/i3/polybar.conf  -r &;
+polybar mybar -c ~/.config/i3/polybar.conf  -r &
 sleep 2;
 polybar mybar-2 -c ~/.config/i3/polybar.conf  -r &
 
